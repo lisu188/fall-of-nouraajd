da88f7cdce4067cdf6507b85c538b85ec051a8eb
Andrzej Lis <andrzej.lis3@gmail.com>
2014-02-07 19:42:34 +0100

    deploy for hudson
    Former-commit-id: 9b28ec55ea851f6835b91ede9f4373a9d58ed406
    Former-commit-id: 89af0d6fc5e3806abd6c4f94eb403ca3b0256279

--- deploy.bat
+++ deploy.bat
@@ -9,21 +9,5 @@ xcopy /s ".\release\Game.exe" ".\official"
 del ..\build.zip
 7za a -tzip ..\build.zip ".\official\*"
 
-rmdir /s /q ".\official"
-rmdir /s /q ".\release"
-rmdir /s /q ".\debug"
-rmdir /s /q ".\save"
 
-astyle.exe -n -xe -j --recursive *.cpp *.h *.json
-
-.\git\git.exe add -A
-.\git\git.exe commit -m auto
-.\git\git.exe clean -fd -x -e *.zip *.apk
-
-mkdir ".\release"
-mkdir ".\debug"
-mkdir ".\save"
-
-del ..\code.zip
-7za a -tzip ..\code.zip ".\*"
 
