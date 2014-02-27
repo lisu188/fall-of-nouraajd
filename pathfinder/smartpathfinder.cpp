169b592c32924654d830fbf0571e86c5de9dc692
Andrzej Lis <andrzej.lis3@gmail.com>
2014-02-27 16:26:00 +0100

    smartpathfinder.cpp
    Former-commit-id: 16f229e0387233040e04985e7ac9efd2fd70aec0
    Former-commit-id: c247bea3bcfd67a3aef74f6fad6b0bfa4d140a42

--- pathfinder/smartpathfinder.cpp
+++ pathfinder/smartpathfinder.cpp
@@ -1,6 +1,7 @@
 #include "dumbpathfinder.h"
 #include "smartpathfinder.h"
-#include <queue>
+#include <set>
+#include <unordered_map>
 #include <QDebug>
 #include <view/gamescene.h>
 #include <configuration/configurationprovider.h>
@@ -23,37 +24,70 @@ Coords SmartPathFinder::findPath(Coords start, Coords goal)
     public:
         int x,y,z;
         Coords goal;
-        Cell(int x,int y,int z,Coords goal):x(x),y(y),z(z),goal(goal){}
+        int level;
+        Cell(int x,int y,int z,Coords goal,int level):x(x),y(y),z(z),goal(goal),level(level){}
     };
     class CellCompare{
     public:
         bool operator()(Cell *a,Cell *b){
-            int dista=std::abs(a->x-a->goal.x)+std::abs(a->y-a->goal.y);
-            int distb=std::abs(b->x-b->goal.x)+std::abs(b->y-b->goal.y);
-            return dista>distb;
+            int dista=(a->x-a->goal.x)*(a->x-a->goal.x)+(a->y-a->goal.y)*(a->y-a->goal.y);
+            int distb=(b->x-b->goal.x)*(b->x-b->goal.x)+(b->y-b->goal.y)*(b->y-b->goal.y);
+            if(dista==distb){
+                return a<b;
+            }
+            return dista<distb;
         }
+    };
+    class FieldQueue :public std::set<Cell*,CellCompare>{
+    private:
+        std::set<Coords> marked;
+        std::unordered_map<Coords,int> prior;
+    public:
+        void mark(int i,int j){
+            marked.insert(Coords(i,j,0));
+        }
+
+        void insert(Cell* cell){
+            if(marked.find(Coords(cell->x,cell->y,0))!=marked.end()){
+                delete cell;
+                return;
+            }
+            if(!((*ConfigurationProvider::getConfig("config/tiles.json"))[GameScene::getGame()->getMap()->getTile(cell->x,cell->y,cell->level)].get("canStep",true).asBool())){
+                delete cell;
+                return;
+            }
+
+        for(FieldQueue::iterator it=begin();it!=end();it++){
+            if(((*it)->x==cell->x)&&((*it)->y==cell->y)&&((*it)->z>cell->z)){
+                delete (*it);
+                erase(it);
+                std::set<Cell*,CellCompare>::insert(cell);
+                return;
+            }
+        }
+        std::set<Cell*,CellCompare>::insert(cell);
+    }
     };
 
     std::list<Cell*> target;
-    std::priority_queue<Cell*,std::deque<Cell*>,CellCompare> queue;
+    FieldQueue queue;
     int level=goal.z;
-    Cell *current=new Cell(goal.x,goal.y,0,start);
-    Map *map=GameScene::getGame()->getMap();
+    Cell *current=new Cell(goal.x,goal.y,0,start,level);
     while(start.x!=current->x||start.y!=current->y){
-        if((*ConfigurationProvider::getConfig("config/tiles.json"))
-                     [map->getTile(current->x,current->y,level)].get("canStep",true).asBool()){
-        queue.push(new Cell(current->x+1,current->y,current->z+1,start));
-        queue.push(new Cell(current->x,current->y+1,current->z+1,start));
-        queue.push(new Cell(current->x-1,current->y,current->z+1,start));
-        queue.push(new Cell(current->x,current->y-1,current->z+1,start));
-}
+
+        queue.insert(new Cell(current->x+1,current->y,current->z+1,start,level));
+        queue.insert(new Cell(current->x,current->y+1,current->z+1,start,level));
+        queue.insert(new Cell(current->x-1,current->y,current->z+1,start,level));
+        queue.insert(new Cell(current->x,current->y-1,current->z+1,start,level));
+
+         queue.mark(current->x,current->y);
          delete current;
-         current=queue.top();
-         queue.pop();
          if(queue.empty())break;
+         current=*queue.begin();
+         queue.erase(queue.begin());
          int dist=std::abs(current->x-start.x)+std::abs(current->y-start.y);
          if(dist==1){
-            target.push_front(current);
+            target.push_front(new Cell(*current));
          }
     }
     if(target.size()==0){
@@ -61,12 +95,21 @@ Coords SmartPathFinder::findPath(Coords start, Coords goal)
         return finder.findPath(start,goal);
     }
     int min=target.front()->z;
+    int x,y;
     Cell *cellmin=target.front();
     for(std::list<Cell*>::iterator it=target.begin();it!=target.end();it++){
         if((*it)->z<min){
             cellmin=*it;
             min=cellmin->z;
+            x=cellmin->x;
+            y=cellmin->y;
         }
+        delete *it;
     }
-    return Coords(cellmin->x-start.x,cellmin->y-start.y,0);
+    target.clear();
+    for(FieldQueue::iterator it=queue.begin();it!=queue.end();it++){
+        delete *it;
+    }
+    queue.clear();
+    return Coords(x-start.x,y-y-start.y,0);
 }
