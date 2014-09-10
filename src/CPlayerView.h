e8b7c3d0e6da989122a0587943a98504342da3cd
Andrzej Lis <andrzej.lis3@gmail.com>
2014-09-11 01:25:25 +0200

    inventory loading
    Former-commit-id: cf45ce21d0b194dfd0bc7966cc5aa150c7ad3706
    Former-commit-id: ccb65f39acb86e4e07bda68152c4289e5ee21efa

--- src/CPlayerView.h
+++ src/CPlayerView.h
@@ -78,3 +78,23 @@ protected:
 	virtual void mousePressEvent ( QGraphicsSceneMouseEvent * );
 };
 
+class CItemSlot : public QGraphicsObject {
+	Q_OBJECT
+public:
+	CItemSlot ( int number, std::map<int, CItem *> *equipped );
+	QRectF boundingRect() const;
+	void paint ( QPainter *painter, const QStyleOptionGraphicsItem *, QWidget * );
+	static bool checkType ( int slot, QObject *widget );
+	void update();
+	int getNumber() { return number; }
+
+protected:
+	void dragMoveEvent ( QGraphicsSceneDragDropEvent *event );
+	void dropEvent ( QGraphicsSceneDragDropEvent *event );
+
+private:
+	int number;
+	QPixmap pixmap;
+	std::map<int, CItem *> *equipped;
+};
+
