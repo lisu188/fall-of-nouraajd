be7f9ca9af9d2f73ab4833a4ef884e71b798eb79
Andrzej Lis <andrzej.lis3@gmail.com>
2014-09-26 10:53:24 +0200

    refactor map object
    Former-commit-id: c045305d15f0842a68894b96a45e34495dc05967
    Former-commit-id: 4e2985e1265f9841a8e4f8ef51fb42013d85f07f

--- CMapObject.cpp
+++ CMapObject.cpp
@@ -124,7 +124,7 @@ CEvent::CEvent ( const CEvent & ) {}
 
 void CEvent::onEnter() {
 	if ( this->isEnabled() ) {
-        CScriptEngine::getInstance()->callFunction ( "events",this->objectName(),boost::ref( *this ) );
+		CScriptEngine::getInstance()->callFunction ( "events",this->objectName(),boost::ref ( *this ) );
 	}
 }
 
