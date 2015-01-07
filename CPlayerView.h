333a2d91122b9033cb19280f5d8b5c19d7a2b8f7
Andrzej Lis <andrzej.lis3@gmail.com>
2015-01-07 01:15:19 +0100

    Build Script
    Former-commit-id: 17e8a87039131e8ba734cc269674b6b12df9487d
    Former-commit-id: ca9bc6c6507193a14b7d05ee5820785813943cce

--- CPlayerView.h
+++ CPlayerView.h
@@ -14,107 +14,106 @@ class CGameView;
 class CTradePanel;
 
 class CPlayerEquippedView : public QGraphicsObject {
-	Q_OBJECT
+    Q_OBJECT
 public:
-	CPlayerEquippedView ( CGameView *view );
-	QRectF boundingRect() const;
-	void paint ( QPainter *, const QStyleOptionGraphicsItem *, QWidget * );
-	void update();
+    CPlayerEquippedView ( CGameView *view );
+    QRectF boundingRect() const;
+    void paint ( QPainter *, const QStyleOptionGraphicsItem *, QWidget * );
+    void update();
 private:
-	std::list<CItemSlot *> itemSlots;
+    std::list<CItemSlot *> itemSlots;
 };
 
 class PlayerStatsView : public QWidget {
-	Q_OBJECT
+    Q_OBJECT
 public:
-	explicit PlayerStatsView();
-	void setPlayer ( CPlayer *value );
-	void update();
+    explicit PlayerStatsView();
+    void setPlayer ( CPlayer *value );
+    void update();
 protected:
-	void paintEvent ( QPaintEvent * );
+    void paintEvent ( QPaintEvent * );
 
 private:
-	CPlayer *player = 0;
+    CPlayer *player = 0;
 };
 
 class CScrollObject;
 class CListView : public QGraphicsObject {
-	Q_OBJECT
-	friend class CScrollObject;
+    Q_OBJECT
+    friend class CScrollObject;
 public:
-	CListView ( CGameView *view );
-	void setDraggable();
-	virtual QRectF boundingRect() const;
-	virtual void paint ( QPainter *painter, const QStyleOptionGraphicsItem *, QWidget * );
-	void updatePosition ( int i );
-	void setXY ( int x, int y );
-	void update();
+    CListView ( CGameView *view );
+    virtual QRectF boundingRect() const;
+    virtual void paint ( QPainter *painter, const QStyleOptionGraphicsItem *, QWidget * );
+    void updatePosition ( int i );
+    void setXY ( int x, int y );
+    void update();
 protected:
-	virtual void dropEvent ( QGraphicsSceneDragDropEvent *event );
-	virtual std::set<QGraphicsItem *> getItems() const=0;
-	CGameView *view=0;
+    virtual void dropEvent ( QGraphicsSceneDragDropEvent *event );
+    virtual std::set<QGraphicsItem *> getItems() const=0;
+    CGameView *view=0;
 private:
-	void setNumber ( QGraphicsItem *item,int i, int x );
-	unsigned int curPosition;
-	unsigned int x, y;
-	CScrollObject *right, *left;
-	QPixmap pixmap;
+    void setNumber ( QGraphicsItem *item,int i, int x );
+    unsigned int curPosition;
+    unsigned int x, y;
+    CScrollObject *right, *left;
+    QPixmap pixmap;
 };
 
 class CPlayerInventoryView:public CListView {
-	Q_OBJECT
+    Q_OBJECT
 public:
-	CPlayerInventoryView ( CGameView *view );
+    CPlayerInventoryView ( CGameView *view );
 protected:
-	virtual std::set<QGraphicsItem *> getItems() const;
+    virtual std::set<QGraphicsItem *> getItems() const;
 };
 
 class CPlayerIteractionView:public CListView {
-	Q_OBJECT
+    Q_OBJECT
 public:
-	CPlayerIteractionView ( CGameView *view );
+    CPlayerIteractionView ( CGameView *view );
 protected:
-	virtual std::set<QGraphicsItem *> getItems() const;
+    virtual std::set<QGraphicsItem *> getItems() const;
 };
 
 class CTradeItemsView : public CListView {
-	Q_OBJECT
+    Q_OBJECT
 public:
-	CTradeItemsView ( CGameView *view );
+    CTradeItemsView ( CGameView *view );
 protected:
-	virtual std::set<QGraphicsItem *> getItems() const;
+    virtual std::set<QGraphicsItem *> getItems() const;
 };
 
 class CScrollObject : public CAnimatedObject {
 public:
-	CScrollObject ( CListView *stats, bool isRight );
-	void setVisible ( bool visible );
+    CScrollObject ( CListView *stats, bool isRight );
+    void setVisible ( bool visible );
 
 private:
-	bool isRight;
-	CListView *stats;
+    bool isRight;
+    CListView *stats;
 
 protected:
-	virtual void mousePressEvent ( QGraphicsSceneMouseEvent * );
+    virtual void mousePressEvent ( QGraphicsSceneMouseEvent * );
 };
 
 class CItemSlot : public QGraphicsObject {
-	Q_OBJECT
+    Q_OBJECT
 public:
-	CItemSlot ( int number, CGameView *view );
-	QRectF boundingRect() const;
-	void paint ( QPainter *painter, const QStyleOptionGraphicsItem *, QWidget * );
-	static bool checkType ( int slot, CItem *item );
-	void update();
-	int getNumber();
+    CItemSlot ( int number, CGameView *view );
+    QRectF boundingRect() const;
+    void paint ( QPainter *painter, const QStyleOptionGraphicsItem *, QWidget * );
+    static bool checkType ( int slot, CItem *item );
+    void update();
+    int getNumber();
 
 protected:
-	void dragMoveEvent ( QGraphicsSceneDragDropEvent *event );
-	void dropEvent ( QGraphicsSceneDragDropEvent *event );
+    void dragMoveEvent ( QGraphicsSceneDragDropEvent *event );
+    void dropEvent ( QGraphicsSceneDragDropEvent *event );
 
 private:
-	int number=-1;
-	QPixmap pixmap;
-	CGameView *view;
+    int number=-1;
+    QPixmap pixmap;
+    CGameView *view;
 };
 
