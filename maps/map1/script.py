eba063511fd3259ee3f6d6a8a28c48d30295749e
Andrzej Lis <andrzej.lis3@gmail.com>
2015-04-11 00:23:38 +0200

    script fix
    Former-commit-id: 169c8504f06c87b9b5985e059157bd0537b50d02

--- maps/map1/script.py
+++ maps/map1/script.py
@@ -9,17 +9,21 @@ completed = False
 class StartEvent(CEvent):
     def onEnter(self,event):
         if event.getCause().isPlayer():
+            self.getMap().getGuiHandler().showMessage(self.getStringProperty('text'))
             self.getMap().removeAll(lambda ob: ob.getStringProperty('objectType')==self.getStringProperty('objectType'))
 
 @game_trigger(event="onDestroy",object="gooby1")
 class GoobyTrigger(CTrigger):
     def trigger(self,object,event):
+        object.getMap().getGuiHandler().showMessage("Gooby killed!!!")
         global completed
         completed=True
 
 @game_trigger(event="onDestroy",object="cave1")
 class CaveTrigger(CTrigger):
     def trigger(self,object,event):
+        object.getMap().getGuiHandler().showMessage("You feel the ground shaking, and see the ratmen all around you!!! But the one part is missing in this puzzle. Letter said about the ratmen who was much bigger than the other. These here are just ordinary pritschers.")
+        gooby=object.getMap().getObjectHandler().createObject("Gooby")
         gooby.setStringProperty("objectName","gooby1")
         object.getMap().addObject(gooby)
         gooby.moveTo(100,100,0)
