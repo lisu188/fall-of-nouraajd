69a9ad6ea4cdf5121d5d157bea8b17f82ca78df6
Andrzej Lis <andrzej.lis3@gmail.com>
2015-05-15 01:33:17 +0200

    hook test

--- Util.h
+++ Util.h
@@ -14,46 +14,49 @@ class CGameObject;
 .def ( "incProperty",&CLASS::incProperty )\
 
 struct Coords {
-	Coords();
-	Coords ( int x, int y, int z );
-	int x, y, z;
-	bool operator== ( const Coords &other ) const;
-	bool operator< ( const Coords &other ) const;
-	Coords operator- ( const Coords &other ) const;
-	int getDist ( Coords a );
+    Coords();
+    Coords ( int x, int y, int z );
+    int x, y, z;
+    bool operator== ( const Coords &other ) const;
+    bool operator< ( const Coords &other ) const;
+    Coords operator- ( const Coords &other ) const;
+    int getDist ( Coords a );
 };
 
 namespace std {
 template<>
 struct hash<Coords> {
-	std::size_t operator() ( const Coords &coords ) const;
+    std::size_t operator() ( const Coords &coords ) const;
 };
 
 template<>
 struct hash<QString> {
-	std::hash<std::string> stringHash;
-	std::size_t operator() ( const QString &string ) const;
+    std::hash<std::string> stringHash;
+    std::size_t operator() ( const QString &string ) const;
 };
 }
 
 class QObject;
 class CObjectData:public QMimeData {
-	Q_OBJECT
+    Q_OBJECT
 public:
-	CGameObject *source;
-	CObjectData ( CGameObject *source );
-	CGameObject *getSource() const;
+    CGameObject *source;
+    CObjectData ( CGameObject *source );
+    CGameObject *getSource() const;
 };
 
 template<typename T,typename U>
 class Lazy {
 public:
-	T*get ( U*parent ) {
-		if ( ptr ) {
-			return ptr;
-		}
-		return ptr=new T ( parent );
-	}
+    T*get ( U*parent ) {
+        if ( ptr ) {
+            return ptr;
+        }
+        return ptr=new T ( parent );
+    }
+    T& operator->(){
+        return *get();
+    }
 private :
-	T*ptr=nullptr;
+    T*ptr=nullptr;
 };
