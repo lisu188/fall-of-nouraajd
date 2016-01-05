f93e06b8adb276c2e8840459794aa20fcd5ec9a5
Andrzej Lis <andrzej.lis3@gmail.com>
2016-01-05 21:27:22 +0100

    merge ctypes

--- src/core/CTypes.cpp
+++ src/core/CTypes.cpp
@@ -1,12 +1,30 @@
 #include "core/CTypes.h"
 
-extern void initialize1();
-
-extern void initialize2();
-
 void CTypes::initialize() {
-    initialize1();
-    initialize2();
+    CTypes::register_type<Stats>();
+    CTypes::register_type<Damage>();
+    CTypes::register_type<CGameObject>();
+    CTypes::register_type<CMapObject>();
+    CTypes::register_type<CWeapon>();
+    CTypes::register_type<CArmor>();
+    CTypes::register_type<CPotion>();
+    CTypes::register_type<CBuilding>();
+    CTypes::register_type<CItem>();
+    CTypes::register_type<CPlayer>();
+    CTypes::register_type<CMonster>();
+    CTypes::register_type<CTile>();
+    CTypes::register_type<CInteraction>();
+    CTypes::register_type<CSmallWeapon>();
+    CTypes::register_type<CHelmet>();
+    CTypes::register_type<CBoots>();
+    CTypes::register_type<CBelt>();
+    CTypes::register_type<CGloves>();
+    CTypes::register_type<CEvent>();
+    CTypes::register_type<CScroll>();
+    CTypes::register_type<CEffect>();
+    CTypes::register_type<CMarket>();
+    CTypes::register_type<CTrigger>();
+    CTypes::register_type<CQuest>();
 }
 
 std::unordered_map<std::string, std::function<std::shared_ptr<CGameObject>() >> &CTypes::builders() {
