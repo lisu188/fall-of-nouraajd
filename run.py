0d79674df84cdf2b7be97db083010e1b09f85032
Andrzej Lis <andrzej.lis3@gmail.com>
2017-02-02 16:21:15 +0100

    some improvements to the start script

--- run.py
+++ run.py
@@ -1,15 +1,22 @@
 #!/usr/bin/python3
 import game
 
-turns = 1000000
-g=game.CGameLoader.loadGame()
-g.setBoolProperty('auto_save', True)
-game.CGameLoader.startGame(g,"map1","Warrior")
-for i in range(turns):
-    g.getMap().move()
-while g.getMap().getNumericProperty('turn') < turns:
-    game.CEventLoop.instance().run()
 
+def advance(game, turns):
+    current_turn = g.getMap().getNumericProperty('turn')
+    for i in range(turns):
+        g.getMap().move()
+    while g.getMap().getNumericProperty('turn') < turns + current_turn:
+        game.CEventLoop.instance().run()
+
+
+if __name__ == '__main__':
+    input()
+    turns = 1000
+    g = game.CGameLoader.loadGame()
+    g.setBoolProperty('auto_save', True)
+    game.CGameLoader.startGame(g, "map1", "Warrior")
+    advance(game, turns)
 # def printName(ob):
 #    print(ob.getStringProperty('name'))
 #
