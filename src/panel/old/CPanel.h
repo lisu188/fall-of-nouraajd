89c2a9d774cc6f7d0df489a96bffb334056dc6c7
Andrzej Lis <andrzej.lis3@gmail.com>
2017-06-07 10:03:46 +0200

    remove old panels

--- src/panel/old/CPanel.h
+++
@@ -1,7 +0,0 @@
-//#pragma once
-//
-//#include "panel/CGamePanel.h"
-//#include "panel/CTextPanel.h"
-//#include "panel/CFightPanel.h"
-//#include "panel/CCharPanel.h"
-//#include "panel/CTradePanel.h"
