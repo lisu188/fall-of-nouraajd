e3c210133900950cd96abe5a8d10433eae49d10e
Andrzej Lis <andrzej.lis3@gmail.com>
2017-06-21 23:20:04 +0200

    reorganize code

--- src/gui/panel/CGameInventoryPanel.h
+++ src/gui/panel/CGameInventoryPanel.h
@@ -10,23 +10,9 @@ V_META(CGameInventoryPanel, CGamePanel,
        V_PROPERTY(CGameInventoryPanel, int, yInv, getYInv, setYInv),
        V_PROPERTY(CGameInventoryPanel, int, selectionBarThickness, getSelectionBarThickness, setSelectionBarThickness))
 
-    void panelMouseEvent(std::shared_ptr<CGui> shared_ptr, int x, int y) override;
-
 public:
     CGameInventoryPanel();
 
-    void panelRender(std::shared_ptr<CGui> shared_ptr, std::shared_ptr<SDL_Rect> pRect, int i) override;
-
-    void panelKeyboardEvent(std::shared_ptr<CGui> shared_ptr, SDL_Keycode i) override;
-
-    bool isInInventory(std::shared_ptr<CGui> x, int y, int i1);
-
-private:
-    int xInv = 0;
-    int yInv = 0;
-
-    int selectionBarThickness = 0;
-public:
     int getXInv();
 
     void setXInv(int xInv);
@@ -40,15 +26,24 @@ public:
     void setSelectionBarThickness(int selectionBarThickness);
 
 private:
-
-
+    int xInv = 0;
+    int yInv = 0;
+    int selectionBarThickness = 0;
     std::weak_ptr<CItem> selected;
+
+    void panelRender(std::shared_ptr<CGui> shared_ptr, std::shared_ptr<SDL_Rect> pRect, int i) override;
+
+    void panelKeyboardEvent(std::shared_ptr<CGui> shared_ptr, SDL_Keycode i) override;
+
+    void panelMouseEvent(std::shared_ptr<CGui> shared_ptr, int x, int y) override;
 
     void drawSelection(std::shared_ptr<CGui> gui, SDL_Rect *location, int thickness);
 
     void drawInventory(std::shared_ptr<CGui> gui, std::shared_ptr<SDL_Rect> pRect, int frameTime);
 
     void drawEquipped(std::shared_ptr<CGui> gui, std::shared_ptr<SDL_Rect> pRect, int frameTime);
+
+    bool isInInventory(std::shared_ptr<CGui> x, int y, int i1);
 
     bool isInEquipped(std::shared_ptr<CGui> shared_ptr, int x, int y);
 
