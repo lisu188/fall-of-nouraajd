61648aee8a38d54ce09426ee37e2d528899aa730
Lis, Andrzej <andrzej.lis3@gmail.com>
2018-05-12 17:00:43 +0200

    support python 3.5

--- configure.sh
+++ configure.sh
@@ -10,6 +10,9 @@ sudo apt install libjsoncpp-dev
 sudo ln -s /usr/lib/x86_64-linux-gnu/libboost_python-py36.a /usr/lib/x86_64-linux-gnu/libboost_python3.a
 sudo ln -s /usr/lib/x86_64-linux-gnu/libboost_python-py36.so /usr/lib/x86_64-linux-gnu/libboost_python3.so
 
+sudo ln -s /usr/lib/x86_64-linux-gnu/libboost_python-py35.a /usr/lib/x86_64-linux-gnu/libboost_python3.a
+sudo ln -s /usr/lib/x86_64-linux-gnu/libboost_python-py35.so /usr/lib/x86_64-linux-gnu/libboost_python3.so
+
 mkdir cmake-build-debug
 mkdir cmake-build-release
 
