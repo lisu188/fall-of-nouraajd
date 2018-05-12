d729c917d0cd197c404f7d07eeea1b00fea389a7
Andrzej Lis <andrzej.lis3@gmail.com>
2018-05-12 13:38:49 +0000

    configure.sh edited online with Bitbucket

--- configure.sh
+++ configure.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-apt update
-apt install build-essential cmake -y
-apt install python3-dev -y
-apt install libboost-dev libboost-system-dev libboost-filesystem-dev libboost-python-dev -y
-apt install libsdl2-dev libsdl2-ttf-dev libsdl2-image-dev -y
-apt install libjsoncpp-dev
+sudo apt update
+sudo apt install build-essential cmake -y
+sudo apt install python3-dev -y
+sudo apt install libboost-dev libboost-system-dev libboost-filesystem-dev libboost-python-dev -y
+sudo apt install libsdl2-dev libsdl2-ttf-dev libsdl2-image-dev -y
+sudo apt install libjsoncpp-dev
 
-ln -s /usr/lib/x86_64-linux-gnu/libboost_python-py36.a /usr/lib/x86_64-linux-gnu/libboost_python3.a
-ln -s /usr/lib/x86_64-linux-gnu/libboost_python-py36.so /usr/lib/x86_64-linux-gnu/libboost_python3.so
+sudo ln -s /usr/lib/x86_64-linux-gnu/libboost_python-py36.a /usr/lib/x86_64-linux-gnu/libboost_python3.a
+sudo ln -s /usr/lib/x86_64-linux-gnu/libboost_python-py36.so /usr/lib/x86_64-linux-gnu/libboost_python3.so
+
+mkdir cmake-build-debug
+mkdir cmake-build-release
+
+cmake -B./cmake-build-debug -H.  -DCMAKE_BUILD_TYPE=Debug -DJSONCPP_ROOT_DIR=/usr/include/jsoncpp
+cmake -B./cmake-build-release -H.  -DCMAKE_BUILD_TYPE=Release -DJSONCPP_ROOT_DIR=/usr/include/jsoncpp
