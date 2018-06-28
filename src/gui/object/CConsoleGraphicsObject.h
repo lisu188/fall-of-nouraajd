e93b89b5dc70ed64c0263a8ad4f9b9c8e858e0b2
Andrzej Lis <andrzej.lis3@gmail.com>
2018-06-28 15:27:45 +0200

    consoleHistory

--- src/gui/object/CConsoleGraphicsObject.h
+++ src/gui/object/CConsoleGraphicsObject.h
@@ -5,7 +5,8 @@
 //TODO: implement history
 class CConsoleGraphicsObject : public CGameGraphicsObject {
 V_META(CConsoleGraphicsObject, CGameGraphicsObject,
-       V_PROPERTY(CConsoleGraphicsObject, std::string, consoleState, getConsoleState, setConsoleState))
+       V_PROPERTY(CConsoleGraphicsObject, std::string, consoleState, getConsoleState, setConsoleState),
+       V_PROPERTY(CConsoleGraphicsObject, std::list<std::string>, consoleHistory, getConsoleHistory, setConsoleHistory))
 //TODO: make this properties
     int height = 30;
     int width = 200;
@@ -19,11 +20,19 @@ public:
 
     void setConsoleState(std::string consoleState);
 
+    std::list<std::string> getConsoleHistory();
+
+    void setConsoleHistory(std::list<std::string> consoleHistory);
+
 private:
     bool inProgress = false;
     std::string consoleState = "";
+    std::vector<std::string> consoleHistory = {""};
+    int historyIndex = 0;
 
     void stopInput();
 
     void startInput();
+
+    void clearConsole();
 };
