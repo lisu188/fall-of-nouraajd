89449be4db9e3a187233cdeea857eba2d8f0e806
Andrzej Lis <andrzej.lis3@gmail.com>
2019-09-27 14:14:23 +0200

    remove boost system install

--- configure.sh
+++ configure.sh
@@ -3,7 +3,7 @@
 sudo apt update
 sudo apt install build-essential cmake -y
 sudo apt install python3-dev -y
-sudo apt install libboost-dev libboost-system-dev libboost-filesystem-dev libboost-python-dev -y
+sudo apt install libboost-dev libboost-filesystem-dev libboost-python-dev -y
 sudo apt install libsdl2-dev libsdl2-ttf-dev libsdl2-image-dev -y
 
 sudo ln -fs /usr/lib/x86_64-linux-gnu/libboost_python-py36.a /usr/lib/x86_64-linux-gnu/libboost_python3.a
