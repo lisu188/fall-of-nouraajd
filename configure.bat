8e5febcec7210756cdb46843c52c63caa45e6492
Lis, Andrzej <andrzej.lis3@gmail.com>
2020-08-20 11:23:59 +0200

    start port

--- configure.bat
+++ configure.bat
@@ -5,10 +5,5 @@ set PATH=windows-tools/Python36/;windows-tools/boost64/;windows-tools/SDL2_image
 mkdir cmake-build-debug
 mkdir cmake-build-release
 
-set CC=D:\a\fall-of-nouraajd\fall-of-nouraajd\windows-tools\mingw\mingw64\bin\gcc.exe
-set CXX=D:\a\fall-of-nouraajd\fall-of-nouraajd\windows-tools\mingw\mingw64\bin\g++.exe
-
-rd /s /q "C:/Program Files (x86)/Microsoft Visual Studio"
-
-cmake -B./cmake-build-debug -H. -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_COMPILER=%CXX% -DCMAKE_C_COMPILER=%CC%
-cmake -B./cmake-build-release -H. -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER=%CXX% -DCMAKE_C_COMPILER=%CC%
+cmake -B./cmake-build-debug -H. -DCMAKE_BUILD_TYPE=Debug -G "MSYS Makefiles"
+cmake -B./cmake-build-release -H. -DCMAKE_BUILD_TYPE=Release -G "MSYS Makefiles"
