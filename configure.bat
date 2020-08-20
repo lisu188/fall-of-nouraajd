7ffc8c037bc3b08e9acb01119df46679134830fc
Lis, Andrzej <andrzej.lis3@gmail.com>
2020-08-20 12:13:13 +0200

    start port

--- configure.bat
+++ configure.bat
@@ -1,9 +1,9 @@
 git clone https://github.com/lisu188/fall-of-nouraajd-windows-tools.git windows-tools
 
-set PATH=windows-tools/Python36/;windows-tools/boost64/;windows-tools/SDL2_image-2.0.5/x86_64-w64-mingw32/;windows-tools/SDL2_ttf-2.0.15/x86_64-w64-mingw32/;windows-tools/SDL2-2.0.12/x86_64-w64-mingw32/;windows-tools/mingw/mingw64/bin/;windows-tools/boost_1_74_0/boost64/lib/;windows-tools/SDL2_image-2.0.5/x86_64-w64-mingw32/bin/;windows-tools/SDL2_ttf-2.0.15/x86_64-w64-mingw32/bin/;windows-tools/SDL2-2.0.12/x86_64-w64-mingw32/bin;C:\Program Files\CMake\bin;
+set PATH=%cd%\windows-tools\Python36\;%cd%\windows-tools\boost64;%cd%\windows-tools\SDL2_image-2.0.5\x86_64-w64-mingw32\;%cd%\windows-tools\SDL2_ttf-2.0.15\x86_64-w64-mingw32\;%cd%\windows-tools\SDL2-2.0.12\x86_64-w64-mingw32\;%cd%\windows-tools\mingw\mingw64\bin\;%cd%\windows-tools\boost_1_74_0\boost64\lib\;%cd%\windows-tools\SDL2_image-2.0.5\x86_64-w64-mingw32\bin\;%cd%\windows-tools\SDL2_ttf-2.0.15\x86_64-w64-mingw32\bin\;%cd%\windows-tools\SDL2-2.0.12\x86_64-w64-mingw32\bin\;%cd%\C:\Program Files\CMake\bin;
 
 mkdir cmake-build-debug
 mkdir cmake-build-release
 
-cmake -B./cmake-build-debug -H. -DCMAKE_BUILD_TYPE=Debug -DCMAKE_MAKE_PROGRAM=D:\\a\\fall-of-nouraajd\\fall-of-nouraajd\\windows-tools\\mingw\\mingw64\\bin\\mingw32-make.exe -G "MinGW Makefiles"
-cmake -B./cmake-build-release -H. -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=%CC% -DCMAKE_MAKE_PROGRAM=D:\\a\\fall-of-nouraajd\\fall-of-nouraajd\\windows-tools\\mingw\\mingw64\\bin\\mingw32-make.exe -G "MinGW Makefiles"
+cmake -B./cmake-build-debug -H. -DCMAKE_BUILD_TYPE=Debug -DCMAKE_MAKE_PROGRAM=mingw32-make.exe -G "MinGW Makefiles"
+cmake -B./cmake-build-release -H. -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=%CC% -DCMAKE_MAKE_PROGRAM=mingw32-make.exe -G "MinGW Makefiles"
