7c058b54b309029830175294db8513d27531aa88
Lis, Andrzej <andrzej.lis3@gmail.com>
2020-08-20 10:34:05 +0200

    start port

--- configure.bat
+++ configure.bat
@@ -1,5 +1,8 @@
-mkdir -p cmake-build-debug
-mkdir -p cmake-build-release
+git clone https://github.com/lisu188/fall-of-nouraajd-windows-tools.git windows-tool
+
+set PATH=windows-tools\Python36\;windows-tools\boost64\;windows-tools\SDL2_image-2.0.5\x86_64-w64-mingw32\;windows-tools\SDL2_ttf-2.0.15\x86_64-w64-mingw32\;windows-tools\SDL2-2.0.12\x86_64-w64-mingw32\;windows-tools\mingw\mingw64\bin\;windows-tools\boost_1_74_0\boost64\lib\;windows-tools\SDL2_image-2.0.5\x86_64-w64-mingw32\bin\;windows-tools\SDL2_ttf-2.0.15\x86_64-w64-mingw32\bin\;windows-tools\SDL2-2.0.12\x86_64-w64-mingw32\bin
+mkdir cmake-build-debug
+mkdir cmake-build-release
 
 cmake -B./cmake-build-debug -H. -DCMAKE_BUILD_TYPE=Debug
 cmake -B./cmake-build-release -H. -DCMAKE_BUILD_TYPE=Release
