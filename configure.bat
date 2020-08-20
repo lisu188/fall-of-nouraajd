81cc098edf466866554b489d084467bf3ac439f7
Lis, Andrzej <andrzej.lis3@gmail.com>
2020-08-20 10:34:17 +0200

    start port

--- configure.bat
+++ configure.bat
@@ -1,4 +1,4 @@
-git clone https://github.com/lisu188/fall-of-nouraajd-windows-tools.git windows-tool
+git clone https://github.com/lisu188/fall-of-nouraajd-windows-tools.git windows-tools
 
 set PATH=windows-tools\Python36\;windows-tools\boost64\;windows-tools\SDL2_image-2.0.5\x86_64-w64-mingw32\;windows-tools\SDL2_ttf-2.0.15\x86_64-w64-mingw32\;windows-tools\SDL2-2.0.12\x86_64-w64-mingw32\;windows-tools\mingw\mingw64\bin\;windows-tools\boost_1_74_0\boost64\lib\;windows-tools\SDL2_image-2.0.5\x86_64-w64-mingw32\bin\;windows-tools\SDL2_ttf-2.0.15\x86_64-w64-mingw32\bin\;windows-tools\SDL2-2.0.12\x86_64-w64-mingw32\bin
 mkdir cmake-build-debug
