1b5d184b2a69adc30511f3ce83b5d9dca3c54185
Lis, Andrzej <andrzej.lis3@gmail.com>
2020-08-21 08:01:11 +0200

    fix path

--- configure.bat
+++ configure.bat
@@ -1,6 +1,6 @@
 git clone https://github.com/lisu188/fall-of-nouraajd-windows-tools.git windows-tools
 
-set PATH=%PATH%;%cd%\windows-tools\Python36\;%cd%\windows-tools\boost64;%cd%\windows-tools\SDL2_image-2.0.5\x86_64-w64-mingw32\;%cd%\windows-tools\SDL2_ttf-2.0.15\x86_64-w64-mingw32\;%cd%\windows-tools\SDL2-2.0.12\x86_64-w64-mingw32\;%cd%\windows-tools\mingw\mingw64\bin\;%cd%\windows-tools\boost_1_74_0\boost64\lib\;%cd%\windows-tools\SDL2_image-2.0.5\x86_64-w64-mingw32\bin\;%cd%\windows-tools\SDL2_ttf-2.0.15\x86_64-w64-mingw32\bin\;%cd%\windows-tools\SDL2-2.0.12\x86_64-w64-mingw32\bin\;
+set PATH=%cd%\windows-tools\Python36\;%cd%\windows-tools\boost64;%cd%\windows-tools\SDL2_image-2.0.5\x86_64-w64-mingw32\;%cd%\windows-tools\SDL2_ttf-2.0.15\x86_64-w64-mingw32\;%cd%\windows-tools\SDL2-2.0.12\x86_64-w64-mingw32\;%cd%\windows-tools\mingw\mingw64\bin\;%cd%\windows-tools\boost_1_74_0\boost64\lib\;%cd%\windows-tools\SDL2_image-2.0.5\x86_64-w64-mingw32\bin\;%cd%\windows-tools\SDL2_ttf-2.0.15\x86_64-w64-mingw32\bin\;%cd%\windows-tools\SDL2-2.0.12\x86_64-w64-mingw32\bin\;C:\Program Files\CMake\bin;
 
 mkdir cmake-build-debug
 mkdir cmake-build-release
