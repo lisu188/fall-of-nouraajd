3ef440a2e19503c4bbb18d3dfb6d73b9d6575a33
Andrzej Lis <andrzej.lis3@gmail.com>
2021-06-19 13:34:46 +0200

    fix config file

--- configure.sh
+++ configure.sh
@@ -6,9 +6,6 @@ sudo apt install python3-dev -y
 sudo apt install libboost-dev libboost-filesystem-dev libboost-python-dev -y
 sudo apt install libsdl2-dev libsdl2-ttf-dev libsdl2-image-dev -y
 
-sudo ln -fs /usr/lib/x86_64-linux-gnu/libboost_python-py36.a /usr/lib/x86_64-linux-gnu/libboost_python3.a
-sudo ln -fs /usr/lib/x86_64-linux-gnu/libboost_python-py36.so /usr/lib/x86_64-linux-gnu/libboost_python3.so
-
 mkdir -p cmake-build-debug
 mkdir -p cmake-build-release
 
