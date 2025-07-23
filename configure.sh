4b327325d798a2dcf2e955a782daf9fdd13830af
lisu188 <andrzej.lis3@gmail.com>
2025-07-23 14:44:37 +0200

    Remove apt upgrade step from configure script (#40)

--- configure.sh
+++ configure.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 sudo apt update
-sudo apt upgrade -y
 sudo apt install build-essential cmake -y
 sudo apt install python3-dev -y
 sudo apt install libboost-dev libboost-filesystem-dev libboost-python-dev -y
