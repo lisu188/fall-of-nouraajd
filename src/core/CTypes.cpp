20af396ff1450aef8516edd5874ddbdc49c60be2
Andrzej Lis <andrzej.lis3@gmail.com>
2026-05-14 15:29:11 +0200

    Add dynamic C++ plugin loading

--- src/core/CTypes.cpp
+++ src/core/CTypes.cpp
@@ -161,9 +161,7 @@ struct register_all_types {
                 CTypes::register_type<CMonsterFightController, CFightController, CGameObject>();
             }
             CTypes::register_type<CGameEvent, CGameObject>();
-            {
-                CTypes::register_type<CGameEventCaused, CGameEvent, CGameObject>();
-            }
+            { CTypes::register_type<CGameEventCaused, CGameEvent, CGameObject>(); }
             CTypes::register_type<CPlugin, CGameObject>();
             {
                 CTypes::register_type<CNativeContentPlugin, CPlugin, CGameObject>();
@@ -177,41 +175,27 @@ struct register_all_types {
             CTypes::register_type<CMapObject, CGameObject>();
             {
                 CTypes::register_type<CBuilding, CMapObject, CGameObject>();
-                {
-                    CTypes::register_type<CWrapper<CBuilding>, CBuilding, CMapObject, CGameObject>();
-                }
+                { CTypes::register_type<CWrapper<CBuilding>, CBuilding, CMapObject, CGameObject>(); }
                 CTypes::register_type<CEvent, CMapObject, CGameObject>();
-                {
-                    CTypes::register_type<CWrapper<CEvent>, CEvent, CMapObject, CGameObject>();
-                }
+                { CTypes::register_type<CWrapper<CEvent>, CEvent, CMapObject, CGameObject>(); }
                 CTypes::register_type<CItem, CMapObject, CGameObject>();
                 {
                     CTypes::register_type<CWeapon, CItem, CMapObject, CGameObject>();
-                    {
-                        CTypes::register_type<CSmallWeapon, CWeapon, CItem, CMapObject, CGameObject>();
-                    }
+                    { CTypes::register_type<CSmallWeapon, CWeapon, CItem, CMapObject, CGameObject>(); }
                     CTypes::register_type<CArmor, CItem, CMapObject, CGameObject>();
                     CTypes::register_type<CPotion, CItem, CMapObject, CGameObject>();
-                    {
-                        CTypes::register_type<CWrapper<CPotion>, CPotion, CItem, CMapObject, CGameObject>();
-                    }
+                    { CTypes::register_type<CWrapper<CPotion>, CPotion, CItem, CMapObject, CGameObject>(); }
                     CTypes::register_type<CHelmet, CItem, CMapObject, CGameObject>();
                     CTypes::register_type<CBoots, CItem, CMapObject, CGameObject>();
                     CTypes::register_type<CBelt, CItem, CMapObject, CGameObject>();
                     CTypes::register_type<CGloves, CItem, CMapObject, CGameObject>();
                     CTypes::register_type<CScroll, CItem, CMapObject, CGameObject>();
-                    {
-                        CTypes::register_type<CWrapper<CScroll>, CScroll, CItem, CMapObject, CGameObject>();
-                    }
+                    { CTypes::register_type<CWrapper<CScroll>, CScroll, CItem, CMapObject, CGameObject>(); }
                 }
-                {
-                    CTypes::register_type<CWrapper<CScroll>, CScroll, CItem, CMapObject, CGameObject>();
-                }
+                { CTypes::register_type<CWrapper<CScroll>, CScroll, CItem, CMapObject, CGameObject>(); }
 
                 CTypes::register_type<CCreature, CMapObject, CGameObject>();
-                {
-                    CTypes::register_type<CPlayer, CCreature, CMapObject, CGameObject>();
-                }
+                { CTypes::register_type<CPlayer, CCreature, CMapObject, CGameObject>(); }
             }
         }
 
@@ -219,40 +203,28 @@ struct register_all_types {
         CTypes::register_type<CGameObject>();
         {
             CTypes::register_type<CEffect, CGameObject>();
-            {
-                CTypes::register_type<CWrapper<CEffect>, CEffect, CGameObject>();
-            }
+            { CTypes::register_type<CWrapper<CEffect>, CEffect, CGameObject>(); }
 
             CTypes::register_type<CMarket, CGameObject>();
             CTypes::register_type<CDialog, CGameObject>();
-            {
-                CTypes::register_type<CWrapper<CDialog>, CDialog, CGameObject>();
-            }
+            { CTypes::register_type<CWrapper<CDialog>, CDialog, CGameObject>(); }
             CTypes::register_type<CDialogOption, CGameObject>();
             CTypes::register_type<CDialogState, CGameObject>();
 
             CTypes::register_type<CTrigger, CGameObject>();
-            {
-                CTypes::register_type<CWrapper<CTrigger>, CTrigger, CGameObject>();
-            }
+            { CTypes::register_type<CWrapper<CTrigger>, CTrigger, CGameObject>(); }
 
             CTypes::register_type<CQuest, CGameObject>();
-            {
-                CTypes::register_type<CWrapper<CQuest>, CQuest, CGameObject>();
-            }
+            { CTypes::register_type<CWrapper<CQuest>, CQuest, CGameObject>(); }
 
             CTypes::register_type<Stats, CGameObject>();
             CTypes::register_type<Damage, CGameObject>();
 
             CTypes::register_type<CTile, CGameObject>();
-            {
-                CTypes::register_type<CWrapper<CTile>, CTile, CGameObject>();
-            }
+            { CTypes::register_type<CWrapper<CTile>, CTile, CGameObject>(); }
 
             CTypes::register_type<CInteraction, CGameObject>();
-            {
-                CTypes::register_type<CWrapper<CInteraction>, CInteraction, CGameObject>();
-            }
+            { CTypes::register_type<CWrapper<CInteraction>, CInteraction, CGameObject>(); }
 
             CTypes::register_type<CController, CGameObject>();
             {
