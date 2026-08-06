fdaac9f45658a3f3f456ad4c98b4337a60271d21
lisu188 <andrzej.lis3@gmail.com>
2026-08-06 20:17:51 +0200

    Implement stat and damage operators

--- src/core/CStats.cpp
+++ src/core/CStats.cpp
@@ -17,6 +17,88 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 #include "core/CStats.h"
 
+#include <cmath>
+
+StatsModifier &StatsModifier::operator+=(const StatsModifier &other) {
+    strength += other.strength;
+    agility += other.agility;
+    stamina += other.stamina;
+    intelligence += other.intelligence;
+    armor += other.armor;
+    block += other.block;
+    dmgMin += other.dmgMin;
+    dmgMax += other.dmgMax;
+    attack += other.attack;
+    hit += other.hit;
+    crit += other.crit;
+    fireResist += other.fireResist;
+    frostResist += other.frostResist;
+    normalResist += other.normalResist;
+    thunderResist += other.thunderResist;
+    shadowResist += other.shadowResist;
+    damage += other.damage;
+    return *this;
+}
+
+StatsModifier &StatsModifier::operator-=(const StatsModifier &other) { return *this += -other; }
+
+StatsModifier StatsModifier::operator-() const {
+    return {
+        .strength = -strength,
+        .agility = -agility,
+        .stamina = -stamina,
+        .intelligence = -intelligence,
+        .armor = -armor,
+        .block = -block,
+        .dmgMin = -dmgMin,
+        .dmgMax = -dmgMax,
+        .attack = -attack,
+        .hit = -hit,
+        .crit = -crit,
+        .fireResist = -fireResist,
+        .frostResist = -frostResist,
+        .normalResist = -normalResist,
+        .thunderResist = -thunderResist,
+        .shadowResist = -shadowResist,
+        .damage = -damage,
+    };
+}
+
+DamageValue &DamageValue::operator+=(const DamageValue &other) {
+    normal += other.normal;
+    fire += other.fire;
+    frost += other.frost;
+    thunder += other.thunder;
+    shadow += other.shadow;
+    return *this;
+}
+
+DamageValue &DamageValue::operator-=(const DamageValue &other) { return *this += -other; }
+
+DamageValue DamageValue::operator-() const {
+    return {
+        .normal = -normal,
+        .fire = -fire,
+        .frost = -frost,
+        .thunder = -thunder,
+        .shadow = -shadow,
+    };
+}
+
+DamageValue &DamageValue::scale(double multiplier) {
+    normal = static_cast<int>(std::lround(normal * multiplier));
+    fire = static_cast<int>(std::lround(fire * multiplier));
+    frost = static_cast<int>(std::lround(frost * multiplier));
+    thunder = static_cast<int>(std::lround(thunder * multiplier));
+    shadow = static_cast<int>(std::lround(shadow * multiplier));
+    return *this;
+}
+
+DamageValue DamageValue::scaled(double multiplier) const {
+    auto result = *this;
+    return result.scale(multiplier);
+}
+
 int CStats::getAttack() const { return attack; }
 
 void CStats::setAttack(int value) { attack = value; }
@@ -93,21 +175,57 @@ int CStats::getMainValue() { return this->getNumericProperty(mainStat); }
 
 CStats::CStats() {}
 
-void CStats::addBonus(std::shared_ptr<CStats> stats) {
-    stats->meta()->for_all_properties(stats, [&](auto property) {
-        if (property->value_type() == std::type_index(typeid(int))) {
-            this->incProperty(property->name(), stats->getProperty<int>(property->name()));
-        }
-    });
+StatsModifier CStats::modifier() const {
+    return {
+        .strength = strength,
+        .agility = agility,
+        .stamina = stamina,
+        .intelligence = intelligence,
+        .armor = armor,
+        .block = block,
+        .dmgMin = dmgMin,
+        .dmgMax = dmgMax,
+        .attack = attack,
+        .hit = hit,
+        .crit = crit,
+        .fireResist = fireResist,
+        .frostResist = frostResist,
+        .normalResist = normalResist,
+        .thunderResist = thunderResist,
+        .shadowResist = shadowResist,
+        .damage = damage,
+    };
 }
 
-void CStats::removeBonus(std::shared_ptr<CStats> stats) {
-    stats->meta()->for_all_properties(stats, [&](auto property) {
-        if (property->value_type() == std::type_index(typeid(int))) {
-            this->incProperty(property->name(), -stats->getProperty<int>(property->name()));
-        }
-    });
+CStats &CStats::apply(const StatsModifier &value) {
+    PropertyNotificationBatch batch(*this);
+    incProperty("strength", value.strength);
+    incProperty("agility", value.agility);
+    incProperty("stamina", value.stamina);
+    incProperty("intelligence", value.intelligence);
+    incProperty("armor", value.armor);
+    incProperty("block", value.block);
+    incProperty("dmgMin", value.dmgMin);
+    incProperty("dmgMax", value.dmgMax);
+    incProperty("attack", value.attack);
+    incProperty("hit", value.hit);
+    incProperty("crit", value.crit);
+    incProperty("fireResist", value.fireResist);
+    incProperty("frostResist", value.frostResist);
+    incProperty("normalResist", value.normalResist);
+    incProperty("thunderResist", value.thunderResist);
+    incProperty("shadowResist", value.shadowResist);
+    incProperty("damage", value.damage);
+    return *this;
 }
+
+CStats &CStats::operator+=(const CStats &other) { return apply(other.modifier()); }
+
+CStats &CStats::operator-=(const CStats &other) { return apply(-other.modifier()); }
+
+void CStats::addBonus(std::shared_ptr<CStats> stats) { *this += *stats; }
+
+void CStats::removeBonus(std::shared_ptr<CStats> stats) { *this -= *stats; }
 
 std::string CStats::getText(int level) {
     std::ostringstream stream;
@@ -125,6 +243,28 @@ std::string CStats::getText(int level) {
 }
 
 CDamage::CDamage() {}
+
+DamageValue CDamage::value() const {
+    return {
+        .normal = normal,
+        .fire = fire,
+        .frost = frost,
+        .thunder = thunder,
+        .shadow = shadow,
+    };
+}
+
+CDamage &CDamage::apply(const DamageValue &value) {
+    PropertyNotificationBatch batch(*this);
+    incProperty("normal", value.normal);
+    incProperty("fire", value.fire);
+    incProperty("frost", value.frost);
+    incProperty("thunder", value.thunder);
+    incProperty("shadow", value.shadow);
+    return *this;
+}
+
+CDamage &CDamage::operator+=(const CDamage &other) { return apply(other.value()); }
 
 int CDamage::getFire() const { return fire; }
 
