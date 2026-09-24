c6c29bcd7c4b4c5597cb78c2531a372a8069b22a
Andrzej Lis <andrzej.lis3@gmail.com>
2026-09-24 09:38:02 +0200

    Use widget declaration for standalone receipt compilation

--- src/gui/panel/CGameLootPanel.cpp
+++ src/gui/panel/CGameLootPanel.cpp
@@ -16,7 +16,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 #include "CGameLootPanel.h"
-#include "CButton.h"
+#include "gui/object/CWidget.h"
 #include <algorithm>
 #include "gui/CLayout.h"
 #include "gui/CTextManager.h"
